23fe3d26d2d737f96a343f499dbd9950bc9e14e4
Karol Golec <developer@awema.pl>
2020-10-19 23:10:07 +0200

    Add footer main layout

--- resources/lang/pl/pages.php
+++ resources/lang/pl/pages.php
@@ -2,6 +2,6 @@
 
 return [
     'main' => [
-        'footer-copyright' => '© :year - Wszelkie prawa zastrzeżone <a href=":link_url">:app_name</a>'
+        'footer-copyright' => '© :year - Wszelkie prawa zastrzeżone <a href=":link_url" target="_blank">:app_name</a>'
     ],
 ];
