eced20d5c2ec09ef2a7bed184fe3f5fedb934f84
Karol Golec <developer@awema.pl>
2020-12-04 13:23:18 +0100

    yes

--- resources/views/chunks/head.blade.php
+++ resources/views/chunks/head.blade.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <title>@yield('meta_title', '')</title>
     <meta name="description" content="@yield('meta_description', '')">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-token" content="xxx{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
     <!-- head block -->
